cd6a2a09ac5c253e73ea7a06b7a08bc02b127a73
yupan <2429862664@qq.com>
2025-08-31 13:58:16 +0800

    feat: 初始化项目结构

---
+++ src/main/java/top/laterya/spring/ai/exploration/SpringAiExplorationApplication.java
@@ -0,0 +1,13 @@
+package top.laterya.spring.ai.exploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExplorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiExplorationApplication.class, args);
+    }
+
+}
